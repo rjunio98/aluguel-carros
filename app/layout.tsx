663702de62aeb76cbad5e5f6ed1f2a6586a44c32
rjunio98 <robledorjunior1@gmail.com>
2024-01-25 06:04:44 -0300

    Add Clerk authentication to layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider, SignIn, SignedIn, SignedOut } from "@clerk/nextjs";
+
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +18,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <SignedIn>{children}</SignedIn>
+          <SignedOut>
+            <SignIn />
+          </SignedOut>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
