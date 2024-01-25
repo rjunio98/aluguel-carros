a006a8d9bd9a72a782c91e8629528abdd68b7c4a
rjunio98 <robledorjunior1@gmail.com>
2024-01-25 06:47:28 -0300

    Remove UserButton component and add h2 element

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>
-    <UserButton/>
-   </div>
+    <div>
+      <h2>fdfdfd</h2>
+    </div>
   );
 }
