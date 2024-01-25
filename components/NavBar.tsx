8914b4e070eab66193e2467498a079022f649ebf
rjunio98 <robledorjunior1@gmail.com>
2024-01-25 06:59:07 -0300

    Add NavBar component

---
+++ components/NavBar.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import React from "react";
+import { UserButton } from "@clerk/nextjs";
+
+function NavBar() {
+  return (
+    <div className="flex items-center justify-between p-3 px-5 shadow-sm border-b-[1px]">
+      <Image
+        src="/logo.png"
+        alt="logo"
+        width={100}
+        height={100}
+      />
+
+      <div className="hidden md:flex gap-5">
+        <h2 className="hover:bg-blue-500 p-2 px-3 cursor-pointer rounded-full hover:text-white">
+          Home
+        </h2>
+        <h2 className="hover:bg-blue-500 p-2 px-3 cursor-pointer rounded-full hover:text-white">
+          History
+        </h2>
+        <h2 className="hover:bg-blue-500 p-2 px-3 cursor-pointer rounded-full hover:text-white">
+          Contact Us
+        </h2>
+      </div>
+      <UserButton />
+    </div>
+  );
+}
+
+export default NavBar;
