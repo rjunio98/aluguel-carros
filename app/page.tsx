5cb8183711fea0a52251a3ea878d9ac6622b3d77
rjunio98 <robledorjunior1@gmail.com>
2024-01-25 07:10:29 -0300

    Add Hero component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+"use client";
+
+import Hero from "@/components/Home/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h2>fdfdfd</h2>
+    <div className="p-5 sm:px-10 md:px-20">
+      <Hero />
     </div>
   );
 }
