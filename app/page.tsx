3da97fc7835ca0b5ac1d6265a53886db7f18b087
rjunio98 <robledorjunior1@gmail.com>
2024-01-26 03:44:51 -0300

    Add SearchInput component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import Hero from "@/components/Home/Hero";
+import SearchInput from "@/components/Home/SearchInput";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="p-5 sm:px-10 md:px-20">
       <Hero />
+      <SearchInput />
     </div>
   );
 }
