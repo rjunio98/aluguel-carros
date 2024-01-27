1b825cde28dce1e94ac4ddef9e567e3748a1cf72
rjunio98 <robledorjunior1@gmail.com>
2024-01-27 11:05:00 -0300

    Add getCarsList function to services/index.ts

---
+++ services/index.ts
@@ -0,0 +1,24 @@
+import { gql, request } from "graphql-request";
+
+export const getCarsList = async () => {
+  const query = gql`
+    query CarLists {
+      carLists {
+        id
+        carAvg
+        createdAt
+        name
+        price
+        publishedAt
+        seat
+        updatedAt
+      }
+    }
+  `;
+
+  const result = await request(
+    "https://api-sa-east-1.hygraph.com/v2/clrvcthhh0djb01te1056xc8x/master",
+    query
+  );
+  return result;
+};
