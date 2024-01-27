ea606ca827f92b6574d6c11a0c29ff3eab57ed80
rjunio98 <robledorjunior1@gmail.com>
2024-01-27 11:04:56 -0300

    Add getCarsList function and fetch cars list on component mount

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,22 @@
 import CarsFiltersOption from "@/components/Home/CarsFiltersOption";
 import Hero from "@/components/Home/Hero";
 import SearchInput from "@/components/Home/SearchInput";
+import { getCarsList } from "@/services";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [carsList, setCarsList] = useState<any>([]);
+
+  useEffect(() => {
+    getCarsList_();
+  }, []);
+
+  const getCarsList_ = async () => {
+    const result: any = await getCarsList();
+    setCarsList(result?.carLists);
+  };
+
   return (
     <div className="p-5 sm:px-10 md:px-20">
       <Hero />
