8f533e18497ecc3aa124e7dc1cae9c389e6823e6
rjunio98 <robledorjunior1@gmail.com>
2024-01-28 06:33:00 -0300

    Add car list ordering functionality

--- app/page.tsx
+++ app/page.tsx
@@ -30,12 +30,20 @@ export default function Home() {
     setCarsList(filterList);
   };
 
+  const orderCarList = (order: any) => {
+    const sortedData = [...carsList].sort((a, b) =>
+      order === -1 ? a.price - b.price : b.price - a.price
+    );
+    setCarsList(sortedData);
+  };
+
   return (
     <div className="p-5 sm:px-10 md:px-20">
       <Hero />
       <SearchInput />
       <CarsFiltersOption
         carsList={carsOrgList}
+        orderCarList={(value: string) => orderCarList(value)}
         setBrand={(value: string) => filterCarList(value)}
       />
       <CarsList carsList={carsList} />
