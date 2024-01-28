d70fd3428657a46eb611af93def211e067351dc2
rjunio98 <robledorjunior1@gmail.com>
2024-01-28 06:52:41 -0300

    Add key prop to CarList items

--- components/Home/CarsList.tsx
+++ components/Home/CarsList.tsx
@@ -5,7 +5,7 @@ function CarsList(props: any) {
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {props.carsList.map((car: any, index: number) => (
-        <div>
+        <div key={index}>
           <CarCard car={car} />
         </div>
       ))}
