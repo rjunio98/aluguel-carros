db905cc99601d0c79993cab0c040d4c189248a4e
rjunio98 <robledorjunior1@gmail.com>
2024-01-28 20:26:43 -0300

    Add new functions and update API URL in services/index.ts

--- services/index.ts
+++ services/index.ts
@@ -1,5 +1,7 @@
 import { gql, request } from "graphql-request";
 
+const MASTER_URL =
+  "https://api-sa-east-1.hygraph.com/v2/clrvcthhh0djb01te1056xc8x/master";
 export const getCarsList = async () => {
   const query = gql`
     query CarLists {
@@ -21,9 +23,57 @@ export const getCarsList = async () => {
     }
   `;
 
-  const result = await request(
-    "https://api-sa-east-1.hygraph.com/v2/clrvcthhh0djb01te1056xc8x/master",
-    query
-  );
+  const result = await request(MASTER_URL, query);
+  return result;
+};
+
+export const getStoreLocations = async () => {
+  const query = gql`
+    query storeLocation {
+      storesLocations {
+        address
+      }
+    }
+  `;
+
+  const result = await request(MASTER_URL, query);
+  return result;
+};
+
+export const createBooking = async (formValue: any) => {
+  const mutationQuery =
+    gql`
+  mutation MyMutation {
+    createBooking(
+      data:  {userName: "` +
+    formValue.userName +
+    `", 
+      pickUpDate: "` +
+    formValue.pickUpDate +
+    `", 
+      pickUpTime: "` +
+    formValue.pickUpTime +
+    `", 
+      dropOffDate: "` +
+    formValue.dropOffDate +
+    `", 
+      dropOffTime: "` +
+    formValue.dropOffTime +
+    `", 
+      contactNumber: "` +
+    formValue.contactNumber +
+    `", 
+      carId: {connect: 
+        {id: "` +
+    formValue.carId +
+    `"}}}
+    ) {
+      id
+    }
+  }
+  
+  `;
+
+  const result = await request(MASTER_URL, mutationQuery);
   return result;
 };
