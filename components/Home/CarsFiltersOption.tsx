dc5e8b60ac38ba10cce82de2ee8a913c291573d4
rjunio98 <robledorjunior1@gmail.com>
2024-01-28 06:52:39 -0300

    Fix brandList initialization in CarsFiltersOption component

--- components/Home/CarsFiltersOption.tsx
+++ components/Home/CarsFiltersOption.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 function CarsFiltersOption({ carsList, setBrand, orderCarList }: any) {
-  const [brandList, setBrandList] = useState<any>([]);
+  const [brandList, setBrandList] = useState<any>();
   const BrandSet = new Set();
 
   useEffect(() => {
@@ -9,25 +9,23 @@ function CarsFiltersOption({ carsList, setBrand, orderCarList }: any) {
       filterCarList();
     }
   }, [carsList]);
-
   const filterCarList = () => {
     carsList.forEach((element: any) => {
       BrandSet.add(element.carBrand);
     });
-
+    console.log(BrandSet);
     setBrandList(Array.from(BrandSet));
   };
-
   return (
     <div className="mt-10 flex items-center justify-between">
       <div>
         <h2 className="text-[30px] font-bold">Cars Catalog</h2>
         <h2>Explore our cars you might likes</h2>
       </div>
-
       <div className="flex gap-5">
         <select
-          className="select select-bordered w-full max-w-xs"
+          className="select select-bordered 
+            w-full max-w-xs"
           onChange={(e) => orderCarList(e.target.value)}
         >
           <option
@@ -37,11 +35,11 @@ function CarsFiltersOption({ carsList, setBrand, orderCarList }: any) {
             Price
           </option>
           <option value={-1}>Min to Max</option>
-          <option value={-1}>Max to Min</option>
+          <option value={1}>Max to Min</option>
         </select>
-
         <select
-          className="select select-bordered w-full md:block max-w-xs hidden"
+          className="select 
+            select-bordered w-full md:block max-w-xs hidden"
           onChange={(e) => setBrand(e.target.value)}
         >
           <option
@@ -50,10 +48,10 @@ function CarsFiltersOption({ carsList, setBrand, orderCarList }: any) {
           >
             Manufactural
           </option>
-
-          {brandList.map((brand: string, index: number) => (
-            <option key={index}>{brand}</option>
-          ))}
+          {brandList &&
+            brandList.map((brand: string, index: number) => (
+              <option key={index}>{brand}</option>
+            ))}
         </select>
       </div>
     </div>
